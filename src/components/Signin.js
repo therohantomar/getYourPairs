e52eab114b4a2c91c65ab9c0b150ebf46246d1da
therohantomar <therohantomar@gmail.com>
2023-10-01 12:43:16 +0530

    user auth login logout authenticated

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -5,17 +5,16 @@ const Signin = () => {
 
     if(isAuthenticated){
         return (
-            <div className='flex items-center gap-4 min-h-screen'>
+            <div className='flex items-center w-full gap-4 min-h-screen'>
             <img src={user?.picture} alt={user?.name} className='rounded-xl' />
-            
-            {!isAuthenticated && <button  className={`btn btn-secondary`} onClick={()=>logout()}>logout</button>}
+             <button  className={`btn btn-secondary`} onClick={()=>logout()}>logout</button>
             </div>
           )
     }
 
  if(!isAuthenticated){
     return (
-        <div className='flex items-center gap-4 min-h-screen'>
+        <div className='flex items-center w-full gap-4 min-h-screen'>
         <h1 className='font-martian font-bold text-xl'>login in socials</h1>
         <button  className={`btn btn-secondary`} onClick={()=>loginWithRedirect()}>login</button>
         </div>
