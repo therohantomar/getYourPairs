b4987b3a6f52b2cde228420a0c04bd178390e212
therohantomar <therohantomar@gmail.com>
2023-10-01 12:41:23 +0530

    user auth login logout authenticated

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -3,11 +3,24 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Signin = () => {
     const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =useAuth0();
 
-  return (
-    <div>
-      <button onClick={()=>loginWithRedirect()}>login</button>
-    </div>
-  )
+    if(isAuthenticated){
+        return (
+            <div className='flex items-center gap-4 min-h-screen'>
+            <img src={user?.picture} alt={user?.name} className='rounded-xl' />
+            
+            {!isAuthenticated && <button  className={`btn btn-secondary`} onClick={()=>logout()}>logout</button>}
+            </div>
+          )
+    }
+
+ if(!isAuthenticated){
+    return (
+        <div className='flex items-center gap-4 min-h-screen'>
+        <h1 className='font-martian font-bold text-xl'>login in socials</h1>
+        <button  className={`btn btn-secondary`} onClick={()=>loginWithRedirect()}>login</button>
+        </div>
+      )
+ }
 }
 
 export default Signin
