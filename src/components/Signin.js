518704e22512c099cf3baddc0eed4dea83c9efdd
therohantomar <therohantomar@gmail.com>
2023-10-01 13:01:21 +0530

    signin page updated

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
 const Signin = () => {
-    const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =useAuth0();
-console.log(user)
+    const { isLoading, isAuthenticated, user, loginWithRedirect, logout } =useAuth0();
+     if(isLoading){
+        return(
+            <div className='flex flex-col items-center justify-center w-full gap-4 min-h-screen'>
+            <span className="loading loading-infinity loading-lg"></span>
+            <h1>Authenticating......</h1>
+            </div>
+        )
+     }
     if(isAuthenticated){
         return (
             <div className='flex flex-col items-center justify-center w-full gap-4 min-h-screen'>
             <img src={user?.picture} alt={user?.name} className='rounded-full' />
-           <ul> {Object.keys(user).map((keys,index)=><li key={index} className=''>{keys}:{user[keys]}</li>)}</ul>
+           <ul> {Object.keys(user).map((keys,index)=><li key={index} className=' font-ubuntu font-bold text-gray-400'>{keys}:{user[keys]}</li>)}</ul>
              <button  className={`btn btn-secondary`} onClick={()=>logout()}>logout</button>
             </div>
           )
