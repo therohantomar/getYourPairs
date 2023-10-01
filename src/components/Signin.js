42ab95db278ba3fe85c23df5c12219ed37d42412
therohantomar <therohantomar@gmail.com>
2023-10-01 12:53:07 +0530

    login details added

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -7,6 +7,7 @@ console.log(user)
         return (
             <div className='flex flex-col items-center justify-center w-full gap-4 min-h-screen'>
             <img src={user?.picture} alt={user?.name} className='rounded-full' />
+           <ul> {Object.keys(user).map((keys,index)=><li key={index} className=''>{keys}:{user[keys]}</li>)}</ul>
              <button  className={`btn btn-secondary`} onClick={()=>logout()}>logout</button>
             </div>
           )
